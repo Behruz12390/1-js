6a8be64309e6d336a1d75546cb6b71b7bab20f8a
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-06-25 11:23:40 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let ism=prompt('Ismingizni kiriting')
+let familiya=prompt('Familiyani kiriting')
+let yosh=prompt('Yoshingizni kiriting')
+let fan=prompt('Yoqtirgan faningizni kiriting')
+let tuman=prompt('Yashidigam tumani kiriting')
+console.log('Ism: ' + ism, '\nFamiliya: ' + familiya, '\nYosh: ' + yosh, '\nYoqtirgan fan: ' + fan, '\nYashidigan tuman: ' + tuman);
+alert('Malumotlarni kiritkaniz uchun rahmat!')
